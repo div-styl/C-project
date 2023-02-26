ee0f557c727d9823f41355d77973fa2acf4e9f7b
musashi <gouhmada@gmail.com>
2023-02-26 01:48:44 +0100

    c-new

--- C/hello.c
+++ C/hello.c
@@ -1,6 +1,26 @@
+
 #include <stdio.h>
-int main (void)
-{
-    puts("hello, world \n");
-    puts("how aare you \n");
-}
+#include <stdlib.h>
+ int main (void)
+ {
+    char Name [] = "john";
+    int age = 20;
+   
+    printf("hello %s \n", Name);
+    printf("hello %d \n", age);
+    char newnamr []= "don"; 
+    age = 21;
+    printf("hello %s \n", newnamr);
+    printf("hello %d \n", age);
+
+    int nwage = 100;
+    double gpa = 4.6;
+    char mark = 'A';
+    char phrase [] = "la fac fuck";
+    printf("age = %d\n", nwage);
+    printf("gpa = %lf\n", gpa);
+    printf("mark = %c\n", mark);
+    printf("phrase = %s\n", phrase);
+    printf ("hello %d,  500");
+    return 0;
+ }
