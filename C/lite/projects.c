6d64df748a9447e62bcf84a2da7df3b70ae5fa74
musashi <gouhmada@gmail.com>
2023-03-02 01:12:17 +0100

    reform

--- C/lite/projects.c
+++ C/lite/projects.c
@@ -2,37 +2,73 @@
 
 int main()
 {
-    int a = 1200;
-    int b = 5;
-    int sum;
-    float presntage;
-    int d = 20;
-    int c = 6;
-    int shortcut = 13;
-// modulo calculation
-    int leftover = d % c;
-// increasing
+   
+ 
+// implicite declaration of function 
+  
+    calculation();
+    modulo();
+    shortcut();
+    icreaing();
+    money();
+ return 0;
+
+ 
+}
+int icreaing ()
+{
    int increas = 1200;
    int decrease = 30;
    increas++;
    decrease--;
-// calculate the sum of a and b
- sum = a * b;
-// shortcut
-shortcut += 2; 
-// calculate tax
+   printf("icrease %d \n",increas);
+   printf("decrease %d \n",decrease);
+   return 0;
+
+}
+int shortcut ()
+{
+    int shortcut = 13;
+    shortcut += 2; 
+
+    printf("shortcut result, %i \n",shortcut);
+    return 0;
+}
+int modulo()
+{
+    int d = 20;
+    int c = 6;
+    int leftover = d % c;
+    printf("what left over = %i \n", leftover);
+    return 0;
+
+}
+
+int calculation ()
+{
+    int a = 1200;
+    int b = 5;
+    int sum;
+    float presntage;
+    sum = a * b;
  presntage = sum * 0.2;
  printf ("result no tax %d \n", sum);
  printf ("reult after tax %.2f \n", presntage);
-//modulo printing
- printf("%i \n", leftover);
-// print increasing 
- printf("icrease %d \n",increas);
- printf("decrease %d \n",decrease);
-// printf the shortcut
-printf("shortcut result, %i",shortcut);
-
- 
  return 0;
- 
+
 }
+
+
+int money ()
+
+{
+    int con = 2;
+    int di = 4;
+
+    if (con != di){
+        con++;
+        printf ("here is it %d \n", con);
+        return 0;
+    }
+}
+
