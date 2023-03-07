5a359b3c7037b80dbdfa308275b8c71d7a3ff2a8
musashi <gouhmada@gmail.com>
2023-03-07 22:41:06 +0100

    new file1

--- C/C-project.c
+++ C/C-project.c
@@ -2,53 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// work place
-/*int jake(){
- double num1, num2, sum;
-char op;
-int task = 1;
-while (task)
-{
-    char name [1000];
-printf ("What's your name: ");
-gets(name);
-printf ("Hello Mr(s) :");
-puts (name);
-    printf (" please enter 1st number : \n ");
-    scanf ("%lf", & num1);
-    
-    printf ("please enter the operation (+,-,/,*) : \n");
-    scanf (" %c", &op);
-    printf (" put the 2nd num :  \n");
-    scanf ("%lf", &num2);
-    switch (op){
-    case '+' :
-    sum = num1 + num2 ;
-    break; 
-    case '-' :
-    sum = num1 - num2;
-    break;
-    case '/' :
-      if (num2 == 0) {
-        printf ("nah it is not going to happen by 0 try another number \n");
-      }else {
-         sum = num1 / num2;
-      }
-    break;
-    case '*' :
-    sum = num1 * num2;
-    break;
-    default :
-    printf ("tfu ach hdshi abndm  \n");
-    break; 
-}
-printf ("the result is : %.lf \n", sum);
-printf ( "enter 1 to redo or  0 to exit : ");
-scanf ("%d", & task);
-}
-return 0;
-}
-*/
+
 void print_result(double nm1, char name [100], double account){
   printf ("Good eveing Mr (%s) \n", name );
   printf ("How much money you would like to put in your bank account sir: ",nm1);
