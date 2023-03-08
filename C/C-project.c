d4ea1c985b2ad81a2e804e3946d2ea3faf8b2155
musashi <gouhmada@gmail.com>
2023-03-08 01:45:38 +0100

    reformof the c project

--- C/C-project.c
+++ C/C-project.c
@@ -5,7 +5,6 @@
 
 void print_result(double nm1, char name [100], double account){
   printf ("Good eveing Mr (%s) \n", name );
-  printf ("How much money you would like to put in your bank account sir: ",nm1);
   printf ("your status bank account is %.2lf \n", account);
 
 }
@@ -13,29 +12,36 @@ int bank()
 {
 double nm1;
 double paywithdraw =1;
-double account = nm1 - paywithdraw ;
+double account;
 double takeout;
 double task = 1;
-int op;
+int op; 
 while (task)
 {
   char name[100];
+  //clean
   printf ("enter name: ");
-  fgets (name, sizeof(name), stdin);
+  scanf ("%S",&name);
   print_result (name);
-  print_result (nm1);
-  scanf ("%.2lf", &nm1);
-
-  printf ("please choose actions you wanna do in your account (withdraw = 1/ accountstatus = 2)"); 
-
+  // odd
+  printf ("please choose actions you wanna do in your account (put= 1 withdraw = 2/ accountstatus = 3)"); 
+  scanf("%s",&op);
         switch (op)
         {
-        case  1 :
+        case 1:
+          printf ("how much y wanna put: ");
+          scanf ("%.2lf",&nm1);
+          printf ("you balance %.2lf", account-1);
+          break;
+        case  2 :
 
             printf (" how much you went to withdraw?: ");
             scanf ("%lf", &takeout);
            account = account - takeout;
             printf ("%.2lf", account );
+            break;
+        case 3 :
+            printf("%.2lf\n", account );
             break;
         default:
             printf ("smthg went wrong press 1 to redo or 0 to exit : ");
