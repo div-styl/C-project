a1e9441952d66cb90d93a8f362b726ba3fffd4a8
musashi <gouhmada@gmail.com>
2023-03-09 01:46:07 +0100

    reformof the c project

--- C/C-project.c
+++ C/C-project.c
@@ -3,8 +3,8 @@
 #include <string.h>
 
 
-void print_result(double nm1, char name [100], double account){
-  printf ("Good eveing Mr (%s) \n", name );
+void printv(double account){
+ 
   printf ("your status bank account is %.2lf \n", account);
 
 }
@@ -18,36 +18,40 @@ double task = 1;
 int op; 
 while (task)
 {
-  char name[100];
-  //clean
-  printf ("enter name: ");
-  scanf ("%S",&name);
-  print_result (name);
-  // odd
-  printf ("please choose actions you wanna do in your account (put= 1 withdraw = 2/ accountstatus = 3)"); 
-  scanf("%s",&op);
+  //get name
+  char name [100];
+   printf("what is your name please? ");
+   scanf ("%s",name);
+   printf("hey Mr. %s\n",name );
+
+  // odds
+  printf ("choose your operation (put= 1 withdraw = 2/ accountstatus = 3): "); 
+  scanf("%d",&op);
         switch (op)
         {
         case 1:
           printf ("how much y wanna put: ");
-          scanf ("%.2lf",&nm1);
-          printf ("you balance %.2lf", account-1);
+          scanf ("%lf",&nm1);
+          account = nm1 - paywithdraw;
+          printf ("you balance %.2lf", account);
           break;
         case  2 :
 
             printf (" how much you went to withdraw?: ");
-            scanf ("%lf", &takeout);
+            scanf ("%.2lf", &takeout);
            account = account - takeout;
             printf ("%.2lf", account );
             break;
         case 3 :
-            print_result(account);
+            printv(account);
             break;
         default:
             printf ("smthg went wrong press 1 to redo or 0 to exit : ");
             scanf ("%lf", &task);
             break;
         }
+
+
 }
 
 return 0;
@@ -56,7 +60,6 @@ return 0;
 
 int main()
 {
- //jake();
  bank();
 
 }
